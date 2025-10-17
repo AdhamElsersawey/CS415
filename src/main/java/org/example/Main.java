0ce60cd0cbd2e09d92bf85e99971c6c2228c4e83
ADHAM <adhamehab235@gmail.com>
2025-10-18 01:20:15 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Calculator.add(1, 2));
+        System.out.println(Calculator.mul(1, 2));
     }
 }
